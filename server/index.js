e64765c9976fc558817041d4741d52d9620faf33
yagiz bingol <yzlsatis@gmail.com>
2025-01-07 04:24:29 +0300

    tes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express ()
 
 app.use("/", (req, res) => {
-    res.send("Sunucu Calisiyor.")
+    res.send("selam")
 })
 
 app.listen(5000, console.log("Sunucu Calisiyor Port 5000"));
